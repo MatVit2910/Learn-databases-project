361c23f2cca4cd21c03e64121abe3a57ef0b7078
Mateo Viteri <mateoviteri13579@gmail.com>
2025-06-27 03:44:10 +0000

    created simple db

--- test.py
+++ test.py
@@ -1,33 +1,17 @@
 import os
 
-from groq import Groq
+import pandas as pd
+import sqlalchemy as db
 import requests
 
 STOCK_DATA_API_KEY = os.environ.get('STOCK_DATA_API_KEY')
-GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
-
-params = {'api_token':STOCK_DATA_API_KEY, 'industries':'Technology', 'language' : 'en'}
-r = requests.get('https://api.stockdata.org/v1/news/all', params=params)
-
-client = Groq(
-    api_key=GROQ_API_KEY,
-)
-
-inp = input("What would you like to ask?\n")
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-        "role": "system",
-        "content": "You are a financial advisor. Keep your responses as concise as possible."
-      },
-      {
-        "role": "user",
-        "content": inp + str(r.json())
-      }
-    ],
-    model="llama-3.3-70b-versatile",
-)
-
-print(chat_completion.choices[0].message.content)
-
+url = 'https://api.stockdata.org/v1/data/quote'
+symbols = 'APPL,GOOG,AMZN'
+params = {'api_token':STOCK_DATA_API_KEY, 'symbols':symbols}
+r = requests.get(url, params=params)
+df = pd.DataFrame.from_dict(r.json()['data'])
+engine = db.create_engine('sqlite:///stocks.db')
+df.to_sql('symbols', con=engine, if_exists='replace', index=False)
+with engine.connect() as c:
+  query_results = c.execute(db.text('SELECT * FROM symbols;')).fetchall()
+  print(pd.DataFrame(query_results))
