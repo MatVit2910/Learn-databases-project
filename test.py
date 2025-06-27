b2e57b7449ae6a23714d78f7acfbc5d76a322261
Mateo Viteri <mateoviteri13579@gmail.com>
2025-06-27 03:58:40 +0000

    updated db

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import os
-
 import pandas as pd
 import sqlalchemy as db
 import requests
 
 STOCK_DATA_API_KEY = os.environ.get('STOCK_DATA_API_KEY')
 url = 'https://api.stockdata.org/v1/data/quote'
-symbols = 'APPL,GOOG,AMZN'
-params = {'api_token':STOCK_DATA_API_KEY, 'symbols':symbols}
+params = {'api_token':STOCK_DATA_API_KEY, 'symbols':'AAPL,GOOG,AMZN'}
 r = requests.get(url, params=params)
-df = pd.DataFrame.from_dict(r.json()['data'])
+data = r.json()['data']
+params['symbols'] = 'TSLA,V,SPY'
+r = requests.get(url, params=params)
+data += r.json()['data']
+params['symbols'] = 'MSFT,NVDA,JPM'
+r = requests.get(url, params=params)
+data += r.json()['data']
+df = pd.DataFrame.from_dict(data)
 engine = db.create_engine('sqlite:///stocks.db')
 df.to_sql('symbols', con=engine, if_exists='replace', index=False)
 with engine.connect() as c:
